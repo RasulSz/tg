dc80d4564b780cccae0326695eb7cddb0f5c1b65
ROG Strix <ROG Strix@ONESECTHESZ>
2023-03-15 01:41:48 +0400

    Add project files.

---
+++ Telegram/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram
+{
+    public class Human : INotifyPropertyChanged
+    {
+        private string? name;
+
+        public string? Name { get => name; set { name = value; RaisePropertyChanged(); } }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Telegram/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Telegram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            DataContext = this;
+
+        }
+
+        private ObservableCollection<Human> People { get;set; } = new()
+        {
+            new Human(){Name="Rasul"},
+            new Human(){Name="Amin"},
+            new Human(){Name="Yura"},
+        };
+
+        private void send_Click(object sender, RoutedEventArgs e)
+        {
+                isent.ItemsSource = new List<string>()
+            {
+                text.Text,
+                "",
+            };
+                text.Text = "";
+                botsent.ItemsSource = new List<string>()
+            {
+                "",
+                "Aleykum Salam"
+            };
+        }
+
+    }
+}
